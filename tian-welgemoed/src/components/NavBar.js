047f24c0c4b08b0a354fb23a9f31061f1287e4e7
Tian Welgemoed <tian.welgemoed@gmail.com>
2023-02-14 13:49:58 +1300

    set up router

---
+++ tian-welgemoed/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./NavBar.css";
+
+export function NavBar() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
